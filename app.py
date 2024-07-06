0d98496f7ac5f0d4fe0b0d0ca89e06ab871a6ed5
omar hafidi alaoui <ohafidi.oh@gmail.com>
2024-07-06 21:43:44 +0100

    init

---
+++ app.py
@@ -0,0 +1,44 @@
+import joblib
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+target_names = ["sadness", "joy", "love", "anger", "fear", "surprise"]
+
+clf = joblib.load('emotion_classifier.joblib')
+
+# Load the saved vectorizer if needed
+vectorizer = joblib.load('tfidf_vectorizer.joblib')
+
+# Function to preprocess and predict a single tweet
+def analyse_tweet(tweet):
+  # Vectorize the tweet using the loaded vectorizer
+  tweet_tfidf = vectorizer.transform([tweet])
+  # Predict the sentiment of the new tweet using the loaded model
+  prediction = clf.predict(tweet_tfidf)
+  predicted_label = target_names[prediction[0]]
+  return predicted_label
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('index.html')
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    text = data.get('text', '')
+    predicted_sentiment = analyse_tweet(text)
+    response = jsonify({'result': predicted_sentiment, 'index': target_names.index(predicted_sentiment)})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/trends', methods=['GET'])
+def trends():
+    return render_template('trends.html')
+@app.route('/trends', methods=['POST'])
+def trendsp():
+    data = request.get_json()
+    text= data.get('text', '')
+    return render_template('trends.html')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/script.js
@@ -0,0 +1,29 @@
+async function fnc() {
+    const tweet = document.getElementById('text').value;
+    const resultDiv = document.getElementById('resultText');
+
+    const emojis = ["😞","😄","😍","😡","😱","😲"]
+
+    // Afficher un message d'attente
+    resultDiv.innerHTML = 'Analyse en cours...';
+
+    try {
+        const response = await fetch('http://127.0.0.1:5000/predict', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text: tweet }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Erreur de réseau');
+        }
+
+        const data = await response.json();
+        const index = data.index;
+        resultDiv.innerHTML = `<p >Sentiment : ${data.result} <span style="font-size:1.8rem;">${emojis[index]}</span></p> `;
+    } catch (error) {
+        resultDiv.innerHTML =' <p style="color: red;">Erreur : ${error.message}</p>';
+    }
+}
